19c215ccce84a13865bbb476fb00315d54fddf11
Janbolot7 <jntrv7.7@gmail.com>
2023-12-27 11:48:09 +0600

    change security Configs

--- src/main/java/kg/startproject/mobimarket_1/configuration/SecurityConfig.java
+++ src/main/java/kg/startproject/mobimarket_1/configuration/SecurityConfig.java
@@ -73,13 +73,12 @@ public class SecurityConfig {
                         "/webjars/**")
                 .permitAll()
 
-//                .antMatchers("/product/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.GET, "/product/**").hasAnyRole("USER", "ADMIN")
-                .antMatchers(HttpMethod.POST, "/product/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.PUT, "/product/**").hasRole("ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/product/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET, "/product/**").hasAnyRole("USER")
+                .antMatchers(HttpMethod.POST, "/product/**").hasRole("USER")
+                .antMatchers(HttpMethod.PUT, "/product/**").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/product/**").hasRole("USER")
 
-                .antMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PUT,"/user/update").hasAnyRole("USER")
 
                 .anyRequest()
                 .authenticated()
