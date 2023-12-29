c94662c6728892c96da51ffe0d7c01afbdddcd42
Janbolot7 <jntrv7.7@gmail.com>
2023-12-29 11:33:11 +0600

    add UserService implemenets in UserServiceImpl

--- src/main/java/kg/startproject/mobimarket_1/service/ServiceImplemintation/UserServiceImpl.java
+++ src/main/java/kg/startproject/mobimarket_1/service/ServiceImplemintation/UserServiceImpl.java
@@ -34,7 +34,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
-public class UserServiceImpl implements UserDetailsService{
+public class UserServiceImpl implements UserDetailsService, UserService{
     @Autowired
     private UserRepository userRepository;
     @Autowired
