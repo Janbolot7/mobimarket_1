c0b5876354165a2fcdd3690522f3b7ca2ee4a0c9
Janbolot7 <jntrv7.7@gmail.com>
2023-12-30 20:18:21 +0600

    jfsklm

--- src/main/java/kg/startproject/mobimarket_1/configuration/SwaggerConfig.java
+++ src/main/java/kg/startproject/mobimarket_1/configuration/SwaggerConfig.java
@@ -16,7 +16,7 @@ import java.util.List;
 public class SwaggerConfig {
     static String BEARER_AUTH = "Bearer";
     public static final String REGISTR = "Регистрация и Аутентификация NEW USER";
-    public static final String USER = "Инструменты for USER 2";
+    public static final String USER = "Инструменты for USER 3";
     public static final String PRODUCT = "Создание нового продукта 1";
 
     @Bean
